78bff643a4b5c1804aa7e46a722921e61cd0d924
Boris Goetz <boris.geotz@gmail.com>
2021-09-23 14:55:11 -0700

    week1.1

--- MyFirstProject/src/boris/teams/Week1.java
+++ MyFirstProject/src/boris/teams/Week1.java
@@ -10,10 +10,16 @@ public class Week1 {
 		String firstName = "Boris";
 		String lastName = "Goetz";
 		String wholeName = firstName + " " + lastName;
+		double accountBalance = 23.70;
 				
 		System.out.println("Hello, my name is wholeName");
 		System.out.println(age);
 		System.out.println("Hello, my name is " + wholeName );
+		System.out.println("Halo, ich heibe " + firstName +" "+ lastName); 
+		System.out.println("I am " + age ); System.out.println(". I am " + age * 8);
+		System.out.println("The definition of love and beauty and wierdness is " + wholeName);
+		System.out.println("Jason takes a bite out of crime and acts like he is " + age/17);
+		System.out.println(accountBalance / 12);
 		
 	}
 
